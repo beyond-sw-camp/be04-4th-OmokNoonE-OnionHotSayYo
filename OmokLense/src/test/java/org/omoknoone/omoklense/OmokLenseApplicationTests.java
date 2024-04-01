9971d5911a227b17fbc495ae9825ac3e5075f322
LAPTOP-FCN2RE9I\Playdata <wkdalstjr94@gmail.com>
2024-04-01 14:38:05 +0900

    [feat] OmokLense 백엔드 기본 프로젝트 세팅

---
+++ OmokLense/src/test/java/org/omoknoone/omoklense/OmokLenseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.omoknoone.omoklense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmokLenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
