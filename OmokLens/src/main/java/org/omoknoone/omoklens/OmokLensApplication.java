42580600d20bfac592590f72304c57c75ec00a87
LAPTOP-FCN2RE9I\Playdata <wkdalstjr94@gmail.com>
2024-04-01 14:51:29 +0900

    [feat] close #1 OmokLens 프로젝트 생성

---
+++ OmokLens/src/main/java/org/omoknoone/omoklens/OmokLensApplication.java
@@ -0,0 +1,13 @@
+package org.omoknoone.omoklens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmokLensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmokLensApplication.class, args);
+    }
+
+}
---
+++ OmokLens/src/test/java/org/omoknoone/omoklens/OmokLensApplicationTests.java
@@ -0,0 +1,13 @@
+package org.omoknoone.omoklens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmokLensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
