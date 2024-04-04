3f5ef2f292b067b6c8834229707e968348a8ebe3
CJC0512 <ehdchs2006@gmail.com>
2024-04-04 16:02:44 +0900

    [feat] table 구현

--- OnionHotSayYo_frontend/src/App.vue
+++ OnionHotSayYo_frontend/src/App.vue
@@ -1,14 +1,19 @@
 <script setup>
 import Sample from "./components/sample.vue";
 
+import Banner from "./components/carousel.vue";
+
+import Table from "./components/table.vue";
 
 </script>
 
 <template>
-  <Sample/>
+  <Banner />
+  <Table />
+  <Sample />
 </template>
 
 
 <style scoped>
-
+@import url(./assets/dist/css/bootstrap.min.css);
 </style>
