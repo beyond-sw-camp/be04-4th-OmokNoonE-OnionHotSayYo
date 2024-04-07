2e3fedcde539f786de5aad52d49de638cc43d91c
CJC0512 <ehdchs2006@gmail.com>
2024-04-07 21:34:41 +0900

    [feat] header profile 서식 추가 2

---
+++ OnionHotSayYo_frontend/src/components/Header/HeaderUpper/HeaderProfile.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="dropdown text-end">
+        <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            <img src="@/assets/image/OmokProfile.png" alt="mdo" width="32" height="32" class="rounded-circle">
+        </a>
+        <ul class="dropdown-menu text-small">
+            <li><a class="dropdown-item" href="#">My Page</a></li>
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+* {
+    font-family: 'NanumBarunGothic' !important;
+
+}
+</style>
