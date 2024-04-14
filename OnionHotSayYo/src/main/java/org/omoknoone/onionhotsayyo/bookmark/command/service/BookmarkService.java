10368c31452c9026bbdaff346b1182c225c0c937
JaewonLee <jlee38266@gmail.com>
2024-04-14 21:58:00 +0900

    #52 [style] bookmark, star 디렉토리의 command 디렉토리 제거
    구분 지어 사용할 이유가 없기에 command 디렉토리만 삭제합니다. 코드나 프로젝트의 구조를 큰 변화를 주지 않기에 style로 간주했습니다!

--- OnionHotSayYo/src/main/java/org/omoknoone/onionhotsayyo/bookmark/command/service/BookmarkService.java
+++
@@ -1,9 +0,0 @@
-package org.omoknoone.onionhotsayyo.bookmark.command.service;
-
-import org.omoknoone.onionhotsayyo.bookmark.command.dto.BookmarkDTO;
-
-public interface BookmarkService {
-	void addBookmark(BookmarkDTO bookmarkDTO);
-
-	void removeBookmark(BookmarkDTO bookmarkDTO);
-}
