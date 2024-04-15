cd291b5f5a760425c6c725cff3f0c7c6bee1b925
jihyeongeun <hyeongeun0924@gmail.com>
2024-04-15 14:01:36 +0900

    [feat]CKEditor 기능 추가

--- OnionHotSayYo_frontend/src/main.js
+++ OnionHotSayYo_frontend/src/main.js
@@ -2,6 +2,7 @@ import './assets/dist/css/bootstrap.min.css'
 import './assets/blog.css'
 import './assets/sign-in.css'
 import './assets/checkout.css'
+import CKEditor from '@ckeditor/ckeditor5-vue';
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -14,7 +15,10 @@ import 'bootstrap';
 import * as ColorModes from "./assets/js/color-modes.js";
 import * as BundleMin from "./assets/dist/js/bootstrap.bundle.min.js";
 
+
 const app = createApp(App);
+// createApp( { /* options */ } ).use( CKEditor ).mount( /* DOM element */ );
+app.use(CKEditor).mount( /* DOM element */ );
 app.config.globalProperties.$axios = axios
 app.use(router);
 app.mount('#app');
