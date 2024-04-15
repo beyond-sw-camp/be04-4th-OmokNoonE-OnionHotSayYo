7842f24779a43ad6c1bee5482ad7e0df21d06655
JaeWon Lee <jlee38266@gmail.com>
2024-04-15 11:25:34 +0900

    #56 [refactor] 충돌 문제 예방을 위한 build.gradle, annotation 추가

--- OnionHotSayYo/src/main/java/org/omoknoone/OnionHotSayYoApplication.java
+++ OnionHotSayYo/src/main/java/org/omoknoone/OnionHotSayYoApplication.java
@@ -5,9 +5,11 @@ import org.modelmapper.config.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class OnionHotSayYoApplication {
 
     public static void main(String[] args) {
