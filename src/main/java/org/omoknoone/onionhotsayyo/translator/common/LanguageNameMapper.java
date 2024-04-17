ce62e40b68cbc5664668cbd4051273f0ca9e519e
ms1011 <wkdalstjr94@gmail.com>
2024-04-17 12:05:12 +0900

    [fix] json 경로 수정

--- src/main/java/org/omoknoone/onionhotsayyo/translator/common/LanguageNameMapper.java
+++ src/main/java/org/omoknoone/onionhotsayyo/translator/common/LanguageNameMapper.java
@@ -10,6 +10,7 @@ import java.util.Iterator;
 import java.util.Map;
 
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,6 +20,26 @@ public class LanguageNameMapper {
 
     static {
         try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            // ClassPathResource를 사용하여 리소스 파일 읽기
+            ClassPathResource resource = new ClassPathResource("languages.json");
+
+            // JSON 파일의 내용을 ObjectMapper를 통해 읽기
+            JsonNode rootNode = objectMapper.readTree(resource.getInputStream());
+
+            // JSON 배열인지 확인
+            if (rootNode.isArray()) {
+                Iterator<JsonNode> elements = rootNode.elements();
+                while (elements.hasNext()) {
+                    JsonNode node = elements.next();
+                    // language와 name을 languageMap에 추가
+                    languageMap.put(node.get("language").asText(), node.get("name").asText());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        /*try {
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode rootNode = objectMapper.readTree(
                 new File("src/main/resources/languages.json"));
@@ -32,7 +53,7 @@ public class LanguageNameMapper {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
     public static String getLanguageName(String languageCode) {
